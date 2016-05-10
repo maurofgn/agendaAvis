c6c7a846bfe4563f3119bee58b1bcc59334edda4
Mauro Fugante <mauro.fugante@gmail.com>
2016-05-10 12:31:52 +0200

    la classe DataTypeFactory maschera il problema di DbUnit il quale ritorna il tipo (UNKNOW) sconosciuto per i booolean quando il db è H2 (per mySQL o altri non ci sarebbe bisogno) H2 ha aggiunto il tipo boolean (nelle versioni precedenti era bit) solo nelle versioni recenti, mentre DBUnit x h2 ancora non si è allineato, è stato previsto a livello di sorgenti, ma questi non sono ancora usati per produrre l'ultima versione ufficiale. Con le versioni future di DBUnit non dovrebbe servire questa classe. La classe EntityDaoImplTest è quella che ne forza l'uso

--- src/test/java/it/mesis/avis/dao/EntityDaoImplTest.java
+++ src/test/java/it/mesis/avis/dao/EntityDaoImplTest.java
@@ -2,6 +2,7 @@ package it.mesis.avis.dao;
 
 import javax.sql.DataSource;
 
+import org.dbunit.database.DatabaseConfig;
 import org.dbunit.database.DatabaseDataSourceConnection;
 import org.dbunit.database.IDatabaseConnection;
 import org.dbunit.dataset.IDataSet;
@@ -12,6 +13,7 @@ import org.springframework.test.context.testng.AbstractTransactionalTestNGSpring
 import org.testng.annotations.BeforeMethod;
 
 import it.mesis.avis.configuration.HibernateTestConfiguration;
+import it.mesis.avis.hsqlDataType.DataTypeFactory;
 
 @ContextConfiguration(classes = { HibernateTestConfiguration.class })
 public abstract class EntityDaoImplTest extends AbstractTransactionalTestNGSpringContextTests {
@@ -22,9 +24,23 @@ public abstract class EntityDaoImplTest extends AbstractTransactionalTestNGSprin
 	@BeforeMethod
 	public void setUp() throws Exception {
 		IDatabaseConnection dbConn = new DatabaseDataSourceConnection(dataSource);
+		
+		DatabaseConfig config = dbConn.getConfig();
+		config.setProperty(DatabaseConfig.PROPERTY_DATATYPE_FACTORY, new DataTypeFactory());
+		
 		DatabaseOperation.CLEAN_INSERT.execute(dbConn, getDataSet());
 	}
 	
 	protected abstract IDataSet getDataSet() throws Exception;
+	
+	
+//	protected IDatabaseConnection getConnection() throws Exception
+//	{
+//	  IDatabaseConnection connection = new DatabaseConnection(dataSource.getConnection());
+//	  DatabaseConfig config = connection.getConfig();
+//	  config.setProperty(DatabaseConfig.PROPERTY_DATATYPE_FACTORY, new HsqlDataTypeFactory());
+//	 
+//	  return connection;
+//	}
 
 }
